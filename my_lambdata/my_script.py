7b707b9158b2064e05a2dd17116ef65c3091b0af
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-06 13:48:18 -0400

    Import functionality from another file

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -3,7 +3,12 @@
 
 import pandas
 
+from my_lambdata.my_mod import enlarge
+
 print("HELLO WORLD")
 
 df = pandas.DataFrame({"state": ["CT", "CO", "CA", "TX"]})
 print(df.head())
+
+
+print(enlarge(9))
